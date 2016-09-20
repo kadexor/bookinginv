00f405cd6e7436a1e055a4f78f61d3575b75c3dc
Angielski Mariusz <mariusz.angielski@jcommerce.pl>
2016-09-20 09:20:19 +0200

    o::

--- infrastructure/test.js
+++ infrastructure/test.js
@@ -3,13 +3,13 @@ var heroin = require('heroin-js');
 var configurator = heroin(process.env.HEROKU_API_TOKEN);
 
 var prod = {
-    name: 'bookinginv',
+    name: 'bookinginv-test',
     organization: undefined,
     region: 'eu',
     maintenance: false,
     stack: 'cedar-14',
     config_vars: {
-        NODE_ENV: 'production',
+        NODE_ENV: 'test',
         MONGODB_URI: process.env.MONGODB_URI
     },
     collaborators: ['makb@gmail.com'],
@@ -24,8 +24,7 @@ var prod = {
         'app-alerting': {enabled: false}
     },
     formation: [{process: 'web', quantity: 1, size: 'Free'}],
-    log_drains: [],
-    domains: ['bookinginv.herokuapp.com']
+    log_drains: []
 };
 
 configurator(prod);
